ccab65b9de33aae490fadd1c93b761ca9cf85f37
Slandio Soares <88685339+Lizandro12@users.noreply.github.com>
2023-07-28 00:00:35 +0100

    finalização da criação do submenu de notificaões

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 
-/* FUNCIONALIDADE QUE EXIBE E ESCONDE O SUBMENU */
+/* FUNCIONALIDADE QUE EXIBE E ESCONDE O SUBMENU do USUÁRIOS */
 
 const userIcon = document.querySelector(".navBar__list__extras-user");
 
@@ -19,4 +19,21 @@ function displaySubmenuOfUsers(){
 function hideSubmenuOfUsers(){
     upIcon.classList.remove("active");
     subMenuOfUsers.classList.remove("active");
+}
+
+/* FUNCIONALIDADE QUE EXIBE E ESCONDE O SUBMENU DAS NOTIFICAÇÕES */
+
+const bellIcon = document.querySelector(".navBar__list-extras-extra");
+
+const subMenuOfNotifications = document.querySelector(".notifications__container")
+
+bellIcon.addEventListener("mouseover",displaySubmenuOfNotifications)
+bellIcon.addEventListener("mouseout",hideSubmenuOfNotifications)
+
+function displaySubmenuOfNotifications(event){
+    subMenuOfNotifications.classList.add("active")
+}
+
+function hideSubmenuOfNotifications(event){
+    subMenuOfNotifications.classList.remove("active")
 }
